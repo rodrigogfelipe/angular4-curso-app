037797ba535fb2c8f48a22b809ead2bc3d3aa911
Rodrigo Felipe <rodrigogfelipe@gmail.com>
2020-02-05 16:43:16 -0300

    Definindo a resposta do usuario (traducao da frase)

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -13,16 +13,18 @@ export class PainelComponent implements OnInit {
   /*Atributos*/
   public frases: Frase[] = FRASES
   public introducao: string = 'Traduza a frase'
+  public resposta: string
 
   constructor() {console.log(this.frases)}
   /*ngOnInit e implementado com interface de um componente*/ 
   ngOnInit() {
   }
-  
-/*Declarando metados*/
-public atualizaReposta(): void {
-  console.log('teste')
 
-}
+/*Declarando metados*/
+public atualizaReposta(resposta: Event): void {
+  this.resposta= ((<HTMLInputElement>resposta.target).value) /*<HTMLInputElement acessar o value da resposta*/
+  console.log(this.resposta)
+
+    } 
 
 }
