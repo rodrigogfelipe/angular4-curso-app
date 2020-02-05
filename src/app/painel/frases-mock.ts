b5ed3e39dee348adb23bd8e631563c3b23f2c1ff
Rodrigo Felipe <rodrigogfelipe@gmail.com>
2020-02-05 14:42:40 -0300

    Criando um mock de obj Frase

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,11 @@
+/*Importando a classe Frase*/
+import{Frase} from '../shared/frase.model'
+
+/*Declarando a variaveis*/
+const FRASES: Frase[] = [
+    {fraseEng:'I like to learn ', frasePtBr: 'Eu gosto de aprender'},
+    {fraseEng:'I watch tv', frasePtBr: 'Eu assisto tv'},
+    {fraseEng:'How are you? ', frasePtBr: 'Como vai você?'},
+    {fraseEng:'I eat bread? ', frasePtBr: 'Eu como pão'},
+
+]
