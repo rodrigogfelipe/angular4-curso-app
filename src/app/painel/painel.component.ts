3f088d44b856461f0c4181d4aa36dd127e8fd62d
Rodrigo Felipe <rodrigogfelipe@gmail.com>
2020-02-05 19:44:14 -0300

    Estabelecendo a logica das rodadas de frases

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -14,14 +14,18 @@ export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES
   public introducao: string = 'Traduza a frase'
   public resposta: string
+  public rodada: number= 0
+  public rodadaFrase: Frase /*classe Frase.model.ts*/
 
-  constructor() {console.log(this.frases)}
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]/*this.rodada e iniciada com valor zero */
+    console.log(this.rodadaFrase)}
   /*ngOnInit e implementado com interface de um componente*/ 
   ngOnInit() {
   }
 
 /*Declarando metados*/
-public atualizaReposta(resposta: Event): void {
+public atualizaResposta(resposta: Event): void {
   this.resposta= ((<HTMLInputElement>resposta.target).value) /*<HTMLInputElement acessar o value da resposta*/
   //console.log(this.resposta)
 
